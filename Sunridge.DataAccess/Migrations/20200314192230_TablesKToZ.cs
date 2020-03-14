4383da441ac5a85e53ab465c3001816a38812d0c
qa <qa@readinghorizons.com>
2020-03-14 13:22:58 -0600

    Migration Stuff

---
+++ Sunridge.DataAccess/Migrations/20200314192230_TablesKToZ.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sunridge.DataAccess.Migrations
+{
+    public partial class TablesKToZ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommonAreaAsset",
+                columns: table => new
+                {
+                    CommonAreaAssetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsArchive = table.Column<bool>(nullable: false),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: false),
+                    AssetName = table.Column<string>(nullable: true),
+                    PurchasePrice = table.Column<float>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommonAreaAsset", x => x.CommonAreaAssetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsItem",
+                columns: table => new
+                {
+                    NewsItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Header = table.Column<string>(maxLength: 50, nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    FileName = table.Column<string>(nullable: true),
+                    FilePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsItem", x => x.NewsItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photo",
+                columns: table => new
+                {
+                    PhotoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerId = table.Column<int>(nullable: false),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photo", x => x.PhotoId);
+                    table.ForeignKey(
+                        name: "FK_Photo_Owner_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owner",
+                        principalColumn: "OwnerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScheduledEvent",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Location = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: true),
+                    IsFullDay = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduledEvent", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maintenance",
+                columns: table => new
+                {
+                    MaintenanceId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsArchive = table.Column<bool>(nullable: false),
+                    LastModifiedBy = table.Column<string>(nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: false),
+                    CommonAreaAssetId = table.Column<int>(nullable: false),
+                    Cost = table.Column<float>(nullable: false),
+                    DateCompleted = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenance", x => x.MaintenanceId);
+                    table.ForeignKey(
+                        name: "FK_Maintenance_CommonAreaAsset_CommonAreaAssetId",
+                        column: x => x.CommonAreaAssetId,
+                        principalTable: "CommonAreaAsset",
+                        principalColumn: "CommonAreaAssetId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenance_CommonAreaAssetId",
+                table: "Maintenance",
+                column: "CommonAreaAssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photo_OwnerId",
+                table: "Photo",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Maintenance");
+
+            migrationBuilder.DropTable(
+                name: "NewsItem");
+
+            migrationBuilder.DropTable(
+                name: "Photo");
+
+            migrationBuilder.DropTable(
+                name: "ScheduledEvent");
+
+            migrationBuilder.DropTable(
+                name: "CommonAreaAsset");
+        }
+    }
+}
