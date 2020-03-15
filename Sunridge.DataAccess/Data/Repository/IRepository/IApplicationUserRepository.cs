8005a8d51e26585aa7c4fd71268d3160117c422a
porter582 <porterokey@mail.weber.edu>
2020-03-15 15:24:45 -0600

    Fix application user

--- Sunridge.DataAccess/Data/Repository/IRepository/IApplicationUserRepository.cs
+++ Sunridge.DataAccess/Data/Repository/IRepository/IApplicationUserRepository.cs
@@ -1,9 +1,13 @@
-﻿using Sunridge.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Sunridge.Models;
+using System.Collections.Generic;
 
 namespace Sunridge.DataAccess.Data.Repository.IRepository
 {
     public interface IApplicationUserRepository : IRepository<ApplicationUser>
     {
+        IEnumerable<SelectListItem> GetApplicationUserListOrDropdown();
 
+        void Update(ApplicationUser applicationUser);
     }
 }
