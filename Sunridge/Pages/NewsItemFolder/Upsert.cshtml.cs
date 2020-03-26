95442e3aff79fe72cd0c471de22185406ef35edf
porter582 <porterokey@mail.weber.edu>
2020-03-26 09:46:41 -0600

    Allow news to now take documents

--- Sunridge/Pages/NewsItemFolder/Upsert.cshtml.cs
+++ Sunridge/Pages/NewsItemFolder/Upsert.cshtml.cs
@@ -41,85 +41,85 @@ namespace Sunridge.Pages.NewsItemFolder
 
         public IActionResult OnPost()
         {
-            //    //find root path wwwroot
-            //    string webRootPath = _hostingEnvironment.WebRootPath;
-            //    //Grab the file(s) from the form
-            //    var files = HttpContext.Request.Form.Files;
+            //find root path wwwroot
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            //Grab the file(s) from the form
+            var files = HttpContext.Request.Form.Files;
 
-            //    //if (!ModelState.IsValid)
-            //    //{
-            //    //    return Page();
-            //    //}
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
 
-            //    if (NewsItemObj.NewsItemId == 0) //new lostandfounditem
-            //    {
-            //        //rename file user submits for image
-            //        string fileName = Guid.NewGuid().ToString();
-            //        //upload file to the path
-            //        var uploads = Path.Combine(webRootPath, @"docs\NewItemFiles");
-            //        //preserve our extension
-            //        var extension = Path.GetExtension(files[0].FileName);
-
-            //        using (var filestream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-            //        {
-            //            files[0].CopyTo(filestream); //files variable comes from the razor page files id
-            //        }
-
-            //        NewsItemObj.FilePath = @"\docs\NewItemFiles\" + fileName + extension;
-
-            //        _unitofWork.NewsItem.Add(NewsItemObj);
-            //    }
-            //    else //else we edit
-            //    {
-            //        var objFromDb =
-            //            _unitofWork.NewsItem.Get(NewsItemObj.NewsItemId);
-            //        //checks if there are files submitted
-            //        if (files.Count > 0)
-            //        {
-            //            //rename file user submits for image
-            //            string fileName = Guid.NewGuid().ToString();
-            //            //upload file to the path
-            //            var uploads = Path.Combine(webRootPath, @"docs\NewItemFiles");
-            //            //preserve our extension
-            //            var extension = Path.GetExtension(files[0].FileName);
-
-            //            var imagePath = Path.Combine(webRootPath, objFromDb.FilePath.TrimStart('\\'));
-
-            //            if (System.IO.File.Exists(imagePath))
-            //            {
-            //                System.IO.File.Delete(imagePath);
-            //            }
-
-            //            using (var filestream = new FileStream(Path.Combine(uploads, fileName, extension), FileMode.Create))
-            //            {
-            //                files[0].CopyTo(filestream);
-            //            }
-
-            //            NewsItemObj.FilePath = @"\docs\NewsItemFiles\" + fileName + extension;
-            //        }
-            //        else
-            //        {
-            //            NewsItemObj.FilePath = objFromDb.FilePath;
-            //        }
-            //        _unitofWork.NewsItem.Update(NewsItemObj);
-            //    }
-            //    _unitofWork.Save();
-            //    return RedirectToPage("./Index");
-            if (!ModelState.IsValid)
+            if (NewsItemObj.NewsItemId == 0) //new lostandfounditem
             {
-                return Page();
-            }
+                //rename file user submits for image
+                string fileName = Guid.NewGuid().ToString();
+                //upload file to the path
+                var uploads = Path.Combine(webRootPath, @"images\newsItems");
+                //preserve our extension
+                var extension = Path.GetExtension(files[0].FileName);
 
-            if (NewsItemObj.NewsItemId == 0) //new category
-            {
+                using (var filestream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream); //files variable comes from the razor page files id
+                }
+
+                NewsItemObj.FilePath = @"\images\newsItems\" + fileName + extension;
+
                 _unitofWork.NewsItem.Add(NewsItemObj);
             }
             else //else we edit
             {
+                var objFromDb =
+                    _unitofWork.NewsItem.Get(NewsItemObj.NewsItemId);
+                //checks if there are files submitted
+                if (files.Count > 0)
+                {
+                    //rename file user submits for image
+                    string fileName = Guid.NewGuid().ToString();
+                    //upload file to the path
+                    var uploads = Path.Combine(webRootPath, @"images\newsItems");
+                    //preserve our extension
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    var imagePath = Path.Combine(webRootPath, objFromDb.FilePath.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+
+                    using (var filestream = new FileStream(Path.Combine(uploads, fileName, extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+
+                    NewsItemObj.FilePath = @"\images\newsItems\" + fileName + extension;
+                }
+                else
+                {
+                    NewsItemObj.FilePath = objFromDb.FilePath;
+                }
                 _unitofWork.NewsItem.Update(NewsItemObj);
             }
             _unitofWork.Save();
             return RedirectToPage("./Index");
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            //if (NewsItemObj.NewsItemId == 0) //new category
+            //{
+            //    _unitofWork.NewsItem.Add(NewsItemObj);
+            //}
+            //else //else we edit
+            //{
+            //    _unitofWork.NewsItem.Update(NewsItemObj);
+            //}
+            //_unitofWork.Save();
+            //return RedirectToPage("./Index");
         }
     }
 }
