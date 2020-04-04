21a2de884b9f8d4f3b73a04e75a3303cc39a9173
Rachel Laub <rachellaub@mail.weber.edu>
2020-04-03 20:30:30 -0600

    Initial calendar changes

--- Sunridge/Pages/Admin/Events/Create.cshtml.cs
+++ Sunridge/Pages/Admin/Events/Create.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SunridgePages.Pages.Admin.Events
+namespace Sunridge
 {
-    public class IndexModel : PageModel
+    public class CreateModel : PageModel
     {
         public void OnGet()
         {
