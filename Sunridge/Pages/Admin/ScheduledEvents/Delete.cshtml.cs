f6f4c1f56c90bd900ca3750f32b3cb6f498a1815
Rachel Laub <rachellaub@mail.weber.edu>
2020-04-07 12:15:41 -0600

    Added CRUD functionality to ScheduledEvents

---
+++ Sunridge/Pages/Admin/ScheduledEvents/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sunridge.Data;
+using Sunridge.Models;
+
+namespace Sunridge.Pages.Admin.ScheduledEvents
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Sunridge.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Sunridge.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ScheduledEvent ScheduledEvent { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ScheduledEvent = await _context.ScheduledEvent.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (ScheduledEvent == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ScheduledEvent = await _context.ScheduledEvent.FindAsync(id);
+
+            if (ScheduledEvent != null)
+            {
+                _context.ScheduledEvent.Remove(ScheduledEvent);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Sunridge/Pages/Admin/ScheduledEvents/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sunridge.Data;
+using Sunridge.Models;
+
+namespace Sunridge.Pages.Admin.ScheduledEvents
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Sunridge.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Sunridge.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ScheduledEvent ScheduledEvent { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ScheduledEvent = await _context.ScheduledEvent.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (ScheduledEvent == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
