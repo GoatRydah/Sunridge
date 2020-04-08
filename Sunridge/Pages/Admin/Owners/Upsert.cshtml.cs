592f4e84c3a24ab2c55a06f21bfa00f07aef5693
qa <qa@readinghorizons.com>
2020-04-07 19:02:16 -0600

    Owners now get default password and are added to owner role upon creation.

--- Sunridge/Pages/Admin/Owners/Upsert.cshtml.cs
+++ Sunridge/Pages/Admin/Owners/Upsert.cshtml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sunridge.DataAccess.Data.Repository.IRepository;
 using Sunridge.Models;
+using Sunridge.Utility;
 
 namespace Sunridge.Pages.Admin.Owners
 {
@@ -15,6 +17,7 @@ namespace Sunridge.Pages.Admin.Owners
     {
         private readonly IUnitOfWork _unitofWork;
         private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly UserManager<IdentityUser> _userManager;
 
         //binds the model to the page
         [BindProperty]
@@ -24,23 +27,48 @@ namespace Sunridge.Pages.Admin.Owners
         [BindProperty]
         public bool createAnother { get; set; }
 
-        public UpsertModel(IUnitOfWork unitofWork, IWebHostEnvironment hostingEnvironment)
+        public UpsertModel(IUnitOfWork unitofWork, IWebHostEnvironment hostingEnvironment, UserManager<IdentityUser> userManager)
         {
             _unitofWork = unitofWork;
             _hostingEnvironment = hostingEnvironment;
+            _userManager = userManager;
         }
 
-        public IActionResult OnGet(string id)
+        public IActionResult OnGet(string id, bool New = false)
         {
+            if (New)
+            {
+                OwnerObj = null;
+                OwnerObj = new ApplicationUser();
 
-            //string userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var temp = _unitofWork.ApplicationUser.GetFirstOrDefault(s => s.Id == id);
 
-            OwnerObj = new ApplicationUser();
-            if (id != null)
-                OwnerObj = _unitofWork.ApplicationUser.GetFirstOrDefault(u => u.Id == id);
-            AddressObj = new Address();
-            if (id != null)
-                AddressObj = _unitofWork.Address.GetFirstOrDefault(u => u.Id == OwnerObj.AddressId);
+                OwnerObj.AddressId = temp.AddressId;
+                OwnerObj.EmergencyContactName = temp.EmergencyContactName;
+                OwnerObj.EmergencyContactPhone = temp.EmergencyContactPhone;
+                
+                var tempAddr = _unitofWork.Address.GetFirstOrDefault(u => u.Id == OwnerObj.AddressId);
+
+                AddressObj = null;
+                AddressObj = new Address();
+
+                AddressObj.StreetAddress = tempAddr.StreetAddress;
+                AddressObj.Apartment = tempAddr.Apartment;
+                AddressObj.City = tempAddr.City;
+                AddressObj.State = tempAddr.State;
+                AddressObj.Zip = tempAddr.Zip;
+            }
+            else
+            {
+                OwnerObj = new ApplicationUser();
+                AddressObj = new Address();
+
+                if (id != null)
+                    OwnerObj = _unitofWork.ApplicationUser.GetFirstOrDefault(u => u.Id == id);
+
+                if (id != null)
+                    AddressObj = _unitofWork.Address.GetFirstOrDefault(u => u.Id == OwnerObj.AddressId);
+            }
 
             return Page();
         }
@@ -63,8 +91,15 @@ namespace Sunridge.Pages.Admin.Owners
                 OwnerObj.AddressId = AddressObj.Id;
                 OwnerObj.Address = AddressObj;
                 OwnerObj.UserName = OwnerObj.FirstName + OwnerObj.LastName;
+                OwnerObj.UserName = OwnerObj.Email;
+
+                //get generic password hash
+               
                 _unitofWork.ApplicationUser.Add(OwnerObj);
                 _unitofWork.Save();
+                var success = AddPassword(OwnerObj, "Temppass123$");
+                if (!success.Result)
+                    return Page();
             }
             else
             {
@@ -74,10 +109,28 @@ namespace Sunridge.Pages.Admin.Owners
 
             if (createAnother)
             {
-                return RedirectToPage("./Upsert");
+                return RedirectToPage("Upsert", "OnGet", new { id = OwnerObj.Id, New = true });
             }
 
             return RedirectToPage("./Index");
+        }
+
+        public async Task<bool> AddPassword(ApplicationUser user, string pass)
+        {
+            var result1 = await _userManager.AddPasswordAsync(user, pass);
+
+            if (!result1.Succeeded)
+                return false;
+
+            var result2 = await _userManager.AddToRoleAsync(user, SD.OwnerRole);
+
+
+            if (!result2.Succeeded)
+                return false;
+
+            _unitofWork.ApplicationUser.Update(user);
+
+            return true;
         }
     }
 }
