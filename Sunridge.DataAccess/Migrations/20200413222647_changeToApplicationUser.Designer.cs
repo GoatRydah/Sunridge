ac53da3a1e2512a5b58bfd89b00031430e764bb3
Mason Rich <masonjamesrich@gmail.com>
2020-04-13 16:27:25 -0600

    added migration

--- Sunridge.DataAccess/Migrations/20200413222647_changeToApplicationUser.Designer.cs
+++ Sunridge.DataAccess/Migrations/20200413222647_changeToApplicationUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sunridge.Data;
 
 namespace Sunridge.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200413222647_changeToApplicationUser")]
+    partial class changeToApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
