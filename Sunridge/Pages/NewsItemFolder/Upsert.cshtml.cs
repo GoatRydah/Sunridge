ad3a0adadd023ef5e9036e260d507616116f9c7a
porter582 <porterokey@mail.weber.edu>
2020-04-13 17:02:23 -0600

    Fix file bug for lost and found and news

--- Sunridge/Pages/NewsItemFolder/Upsert.cshtml.cs
+++ Sunridge/Pages/NewsItemFolder/Upsert.cshtml.cs
@@ -83,21 +83,25 @@ namespace Sunridge.Pages.NewsItemFolder
                 //checks if there are files submitted
                 if (files.Count > 0)
                 {
-                    //rename file user submits for image
+                    //rename the file the user submits
+                    //guid is a unique string that will not be duplicated
                     string fileName = Guid.NewGuid().ToString();
-                    //upload file to the path
+
+                    //upload to path
                     var uploads = Path.Combine(webRootPath, @"images\newsItems");
+
                     //preserve our extension
                     var extension = Path.GetExtension(files[0].FileName);
-
                     var imagePath = Path.Combine(webRootPath, objFromDb.FilePath.TrimStart('\\'));
 
+                    //if image already exists
                     if (System.IO.File.Exists(imagePath))
                     {
                         System.IO.File.Delete(imagePath);
                     }
 
-                    using (var filestream = new FileStream(Path.Combine(uploads, fileName, extension), FileMode.Create))
+                    //append new name to the extension
+                    using (var filestream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         files[0].CopyTo(filestream);
                     }
