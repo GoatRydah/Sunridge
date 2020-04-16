2451f12e9b7c10acab175f725eda36b3c5a591b8
qa <qa@readinghorizons.com>
2020-04-16 12:51:55 -0600

    validation for owners upsert page

--- Sunridge.Models/ViewModels/ClassifiedListingViewModel.cs
+++ Sunridge.Models/ViewModels/ClassifiedListingViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Sunridge.Models.ViewModels
 {
     //for admin
+    [NotMapped]
     public class ClassifiedListingViewModel
     {
         public int ClassifiedListingViewModelId { get; set; }
