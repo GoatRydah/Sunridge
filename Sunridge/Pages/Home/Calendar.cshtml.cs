37ec1f62bdbee54c2155103a5831ebee6b309024
Rachel Laub <rachellaub@mail.weber.edu>
2020-04-16 08:26:51 -0600

    Update on home pages

--- Sunridge/Pages/Home/Calendar.cshtml.cs
+++ Sunridge/Pages/Home/Calendar.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sunridge
+namespace Sunridge.Pages.Home
 {
     public class CalendarModel : PageModel
     {
--- Sunridge/Pages/Home/FireInfo.cshtml.cs
+++ Sunridge/Pages/Home/FireInfo.cshtml.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Sunridge
+namespace Sunridge.Pages.Home
+
 {
     public class FireInfoModel : PageModel
     {
