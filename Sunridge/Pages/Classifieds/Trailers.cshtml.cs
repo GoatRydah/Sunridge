4d4a2b49712c3d13be7b32ef7fa76db3591267dd
Rachel Laub <rachellaub@mail.weber.edu>
2020-04-16 08:11:07 -0600

    Added Classified items

---
+++ Sunridge/Pages/Classifieds/Trailers.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sunridge.DataAccess.Data.Repository.IRepository;
+using Sunridge.Models;
+
+namespace Sunridge.Pages.Classifieds
+{
+    public class TrailersModel : PageModel
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public TrailersModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<ClassifiedListing> ClassifiedListingList { get; set; }
+
+        public void OnGet()
+        {
+            ClassifiedListingList = _unitOfWork.ClassifiedListing.GetAll();
+
+        }
+    }
+}
