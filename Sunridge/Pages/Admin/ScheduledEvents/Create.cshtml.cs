1a99ef90eedf9a6dc4a5ab12e72dd6b4ecfecc44
Rachel Laub <rachellaub@mail.weber.edu>
2020-04-17 10:11:25 -0600

    Added real data

--- Sunridge/Pages/Admin/ScheduledEvents/Create.cshtml.cs
+++ Sunridge/Pages/Admin/ScheduledEvents/Create.cshtml.cs
@@ -57,76 +57,7 @@ namespace Sunridge.Pages.Admin.ScheduledEvents
                 _unitofWork.ScheduledEvents.Update(ScheduledEventObj);
             }
 
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-            if (claim != null)
-            {
-
-                //find root path wwwroot
-                string webRootPath = _hostingEnvironment.WebRootPath;
-                //Grab the file(s) from the form
-                var files = HttpContext.Request.Form.Files;
-
-                if (!ModelState.IsValid)
-                {
-                    return Page();
-                }
-
-                if (ScheduledEventObj.ID == 0) //new 
-                {
-                    //rename file user submits for image
-                    string fileName = Guid.NewGuid().ToString();
-                    //upload file to the path
-                    var uploads = Path.Combine(webRootPath, @"images\ScheduledEvents");
-                    //preserve our extension
-                    var extension = Path.GetExtension(files[0].FileName);
-
-                    using (var filestream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(filestream); //files variable comes from the razor page files id
-                    }
-
-                    ScheduledEventObj.Image = @"\images\ScheduledEvents\" + fileName + extension;
-
-                    _unitofWork.ScheduledEvents.Add(ScheduledEventObj);
-                }
-                else //else we edit
-                {
-                    var objFromDb =
-                        _unitofWork.ScheduledEvents.Get(ScheduledEventObj.ID);
-                    //checks if there are files submitted
-                    if (files.Count > 0)
-                    {
-                        //rename file user submits for image
-                        string fileName = Guid.NewGuid().ToString();
-                        //upload file to the path
-                        var uploads = Path.Combine(webRootPath, @"images\ScheduledEvents");
-                        //preserve our extension
-                        var extension = Path.GetExtension(files[0].FileName);
-
-                        var imagePath = Path.Combine(webRootPath, objFromDb.Image.TrimStart('\\'));
-
-                        if (System.IO.File.Exists(imagePath))
-                        {
-                            System.IO.File.Delete(imagePath);
-                        }
-
-                        using (var filestream = new FileStream(Path.Combine(uploads, fileName, extension), FileMode.Create))
-                        {
-                            files[0].CopyTo(filestream);
-                        }
-
-                        ScheduledEventObj.Image = @"\images\ScheduledEvents\" + fileName + extension;
-                    }
-                    else
-                    {
-                        ScheduledEventObj.Image = objFromDb.Image;
-                    }
-
-                    _unitofWork.ScheduledEvents.Update(ScheduledEventObj);
-                }
-            }
+           
             _unitofWork.Save();
             return RedirectToPage("./Index");
         }
